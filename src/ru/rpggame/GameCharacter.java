17fc62fde1d232b49ddf67b5ded422d5ce63c956
Abilis <abilis@mail.ru>
2016-03-07 00:26:44 +0400

    Промежуточный коммит

--- src/ru/rpggame/GameCharacter.java
+++ src/ru/rpggame/GameCharacter.java
@@ -1,15 +1,25 @@
 package ru.rpggame;
 
+import java.util.Objects;
+
 /**
  * Created by Abilis on 06.03.2016.
  */
-public class GameCharacter {
+public class GameCharacter implements Cloneable {
 
     protected String charClass;
     protected String name;
+
+    public String getName() {
+        return name;
+    }
+
     protected int hp;
+    protected int hpMax;
     protected int attack;
     protected int defense;
+
+    protected boolean blockStanse;
 
 
     public GameCharacter(String charClass, String name, int hp, int attack, int defense) {
@@ -17,9 +27,30 @@ public class GameCharacter {
         this.charClass = charClass;
         this.name = name;
         this.hp = hp;
+        this.hpMax = hp;
         this.attack = attack;
         this.defense = defense;
 
+    }
+
+    public Object clone() {
+
+        try {
+            return super.clone();
+        }
+
+        catch (CloneNotSupportedException e) {
+            System.out.println("Клонирование невозможно!");
+            return this;
+        }
+    }
+
+    public void showInfo() {
+        System.out.println("Имя: " + name + ", hp: " + hp + "/" + hpMax);
+    }
+
+    public void makeNewRound() {
+        blockStanse = false;
     }
 
 
--- src/ru/rpggame/GameLogic.java
+++ src/ru/rpggame/GameLogic.java
@@ -1,5 +1,10 @@
 package ru.rpggame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by Abilis on 06.03.2016.
  */
@@ -13,9 +18,58 @@ public class GameLogic {
 
     private int currentRound;
 
+    private String strNumHero;
+
+    public GameLogic() {
+        initGame();
+    }
+
 
     public void mainGameLoop() { //основная игровая логика
 
+        InputStream inputStream = System.in;
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+
+
+        System.out.println("Игра началась!");
+        System.out.println("Выберите героя:");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println((i + 1) + ". " + heroPattern[i].getName());
+        }
+
+        try {
+            this.strNumHero = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int numHero = Integer.parseInt(strNumHero); //считываем введенное пользователем числов
+
+        mainHero = (Hero)heroPattern[numHero - 1].clone(); //создаем героя путем копирования из шаблона
+        System.out.println("Вы выбрали героя " + mainHero.getName());
+
+        currentMonster = (Monster)monsterPattern[0].clone(); //создаем монстра путем копирования из шаблона
+
+        do {
+            //выводим текущую информацию о игроке и противнике в начале каждого раунда
+            mainHero.showInfo();
+            currentMonster.showInfo();
+
+            //ход игрока
+            System.out.println("Ход игрока. 1 - атака, 2 - защита, 3 - пропустить ход, 9 - выйти из игры");
+            mainHero.makeNewRound(); //сбрасываем параметры для начала нового раунда
+
+            
+
+
+            break;
+
+
+        } while (true);
 
 
 
