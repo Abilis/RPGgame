5d23ed77815afa3b9ade7266639bcd8c26d33f99
Abilis <abilis@mail.ru>
2016-03-07 01:55:25 +0400

    Подкручены переменные

--- src/ru/rpggame/GameLogic.java
+++ src/ru/rpggame/GameLogic.java
@@ -138,9 +138,9 @@ public class GameLogic {
 
     public void initGame() { //инициализируется начальное состояние игры
         //задаются шаблоны для главного героя и монстров
-        heroPattern[0] = new Hero("Рыцарь", "Великий рыцарь", 600, 30, 12);
+        heroPattern[0] = new Hero("Рыцарь", "Великий рыцарь", 500, 30, 12);
         heroPattern[1] = new Hero("Варвар", "Конан", 600, 50, 0);
-        heroPattern[2] = new Hero("Дворф", "Гимли", 600, 20, 25);
+        heroPattern[2] = new Hero("Дворф", "Гимли", 400, 20, 30);
 
         monsterPattern[0] = new Monster("Гуманоид", "Злобный гоблин", 120, 30, 2);
         monsterPattern[1] = new Monster("Гуманоид", "Сильный орк", 240, 50, 2);
